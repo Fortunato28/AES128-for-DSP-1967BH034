1359e4a9d9aed6d4e3d919902a5f9fca6d618894
Fortunato28 <fort.sav.28@gmail.com>
2017-08-11 11:40:44 +0700

    fix last block, bad padding

--- encriptData.c
+++ encriptData.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include "aes128.h"
 
-#define LENGTH 80 // длина открытого текста
+#define LENGTH 77 // длина открытого текста
+//74
 
-void encriptData(void *opentext, int len, void *ciphertext); // Хз, как тут с типами указателей
+int encriptData(void *opentext, int len); // Хз, как тут с типами указателей
+int getCipherLength(int len);
  
+
+/**
+  * @brief  Зашифрование области данных
+  * @param  -, однако нужно иметь указатель на область данных и их длину
+  * @retval -
+  */
 void main( void )
 {
-	uint8_t data[] = {0xfa, 0x8d, 0x03, 0x5d, 0xde, 0x0e, 0x32, 0xfa, 0xab, 0x7c, 0xb7, 0xbf, 0xd9, 0xa0, 0x7a, 0xaf, 0x27, 0x92, 0x80, 0x67, 0x01, 0x73, 0x26, 0x25, 0x40, 0x57, 0x62, 0x86, 0x21, 0x52, 0x49, 0x24, 0x04, 0x26, 0x73, 0x28, 0x32, 0x17, 0x65, 0x10, 0x27, 0x92, 0x45, 0x92, 0x50, 0x92, 0x76, 0x83, 0x72, 0x65, 0x40, 0x27, 0x96, 0x21, 0x67, 0x62, 0x17, 0x96, 0x52, 0x49, 0x25, 0x78, 0x76, 0x65, 0x42, 0x04, 0x62, 0x97, 0x84, 0x94, 0x23, 0x49, 0x09, 0x27};
-	int len = LENGTH;
+ 	uint8_t data[] = {0xfa, 0x8d, 0x03, 0x5d, 0xde, 0x0e, 0x32, 0xfa, 0xab, 0x7c, 0xb7, 0xbf, 0xd9, 0xa0, 0x7a, 0xaf, 0x13, 0x3c, 0x86, 0xcd, 0xc6, 0xf6, 0xe6, 0x41, 0x4a, 0x12, 0xf8, 0x7c, 0x26, 0x61, 0xfc, 0x38, 0xbe, 0xd7, 0x71, 0x88, 0x32, 0xaf, 0x6c, 0xbe, 0x57, 0x89, 0x71, 0x60, 0x61, 0x48, 0x69, 0x60, 0xaa, 0x3e, 0xb6, 0xf8, 0xe6, 0x5f, 0xd1, 0x1b, 0x80, 0x57, 0xfc, 0x92, 0x6f, 0xbb, 0xb8, 0xa8, 0x71, 0x32, 0x43, 0x5a, 0x15, 0xb1, 0x56, 0x6f, 0xda, 0xc9,/**/ 0x34, 0xd4, 0x2c};
+ 	int len = LENGTH, j;
 	uint8_t *ciphertext;
 	
+	int cipherLength = getCipherLength(len); // Длина шифротекста
+	ciphertext = encriptData(data, len);
 	
-	encriptData(data, len, ciphertext);
+	for(j = 0; j < cipherLength; ++j)
+			printf("%x\n", *(ciphertext + j));
 	
 		while(1);
 }
@@ -44,39 +54,82 @@ int getCipherLength(int len)
 
 /**
   * @brief  Шифрование блока данных произвольной длины
-  * @param  Указатель на начало открытого текста opentext, длина открытого текста len, 
-  					указатель на начало области для записи шифротекста ciphertext, ключ шифрования key
-  * @retval Блок памяти с зашифрованными данными
+  * @param  Указатель на начало открытого текста opentext, длина открытого текста len
+  * @retval Указатель на блок памяти с зашифрованными данными
   */
-void encriptData(void *opentext, int len, void *ciphertext) // нужно здесь указывать, что ссылки на массив?
+int encriptData(void *opentext, int len) // нужно здесь указывать, что ссылки на массив?
 {
 	uint8_t key[4 * Nk] = {0x66, 0xa3, 0x6c, 0x8b, 0x8c, 0x54, 0x92, 0x52, 0x7c, 0xd7, 0x6b, 0x78, 0xfc, 0x67, 0x7d, 0x12};
-	
-	//а вот бля не факт
-	int numberOfBlocks = ceil((double)len / 16); // Количество блоков для зашифрования
-	
-	int i, j;
-	
 	int w[Nb * (Nr + 1)]; // Расписание ключей
 	keyExpansion(key, w); // Расширение ключа
 	
+	uint8_t *ciphertext;
+	int i, j;
+	uint8_t buff[4 * Nb]; // Буфер для блока открытых данных
+	uint8_t blockShortageUpToMultiplicity = 16 - (len % 16); // Недостаток последнего блока до кратности 128
+	
 	int cipherLength = getCipherLength(len);
-	ciphertext = (uint8_t*)malloc(cipherLength); // Ну вот не совсем так, надо ещё функцию для дополнения
+	int numberOfBlocks = cipherLength / 16; // Количество блоков шифротекста
+	ciphertext = malloc(cipherLength); // Количество памяти, выделяемой под блок шифротекста кратно 128 битам
 	
-	for(i = 0; i < 1/*numberOfBlocks*/; ++i) // Пока разочек, а вообще по всем блокам должен пройти
+
+	// Идём по блокам
+	for(i = 0; i < numberOfBlocks; ++i) // Шифруем каждый блок отдельно
 	{
-		encript_block(opentext, ciphertext, w);
+		// Для последнего блока данных осуществляем дополнение
+		if(i == numberOfBlocks - 1)
+		{
+			if(blockShortageUpToMultiplicity == 16) // Длина сообщения кратна 128
+			{
+				memset(buff, 0x10, 16); // Заполняем последний блок 				
+			}
+			else
+			{
+				for(j = 0; j < len % 16; ++j)
+				{
+					memcpy(buff, opentext, len % 16);
+					//memset(buff + j, *(uint8_t *)(opentext + j), len % 16);
+				}
+				memset(buff + (len % 16), blockShortageUpToMultiplicity, blockShortageUpToMultiplicity); // Заполняем остатки блока
+			}
+		}
 		
-		for(i = 0; i < 16; ++i)
-			printf("%x", *(uint8_t *)opentext++);
-	
-			printf("\n%d\n", cipherLength);
+		
+		// Шифруем
+		if(i != numberOfBlocks - 1)
+		{
+			encript_block(opentext, ciphertext, w);
+		}
+		else
+		{
 			
-		for(i = 0; i < 16; ++i)
+			/*
+			printf("Test buff\n");
+			for(j = 0; j < 16; ++j)
+				printf("%x", *(buff + j));
+				*/
+			encript_block(buff, ciphertext, w); // Уходит всё правильно
+		}
+		
+		/*for(j = 0; j < 16; ++j)
+		{
+			if(i != numberOfBlocks - 1)
+				printf("%x", *(uint8_t *)opentext++);
+			else
+				printf("%x", *(buff + j));
+		}
+		
+			printf("\ncipherLength = %d\n", cipherLength);
+			
+		for(j = 0; j < 16; ++j)
 			printf("%x", *(uint8_t *)ciphertext++);
 			
-		//ciphertext += 16;
+		printf("\ncipherLength = %d\n", cipherLength);*/
+			
+		opentext += 16; // Смещение указателей для работы со следующим блоком данных
+		ciphertext += 16;
 	}
+	return ciphertext - cipherLength;
 }
 
 
